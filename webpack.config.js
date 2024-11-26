66a97a075d30e9083cfa63112a7004ee2eb076b1
Vasanth <vasanth.n.kumar@accenture.com>
2024-11-26 17:10:53 +0800

    change

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: "./src/index.tsx",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
     clean: true, // Clean the output directory before every build
   },
